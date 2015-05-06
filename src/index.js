5f51a194fa1ad4c960a2427120186fbeb52475ca
acnalesso <antonio.nalessomoreira@bskyb.com>
2015-05-06 12:42:47 +0100

    AN: initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require("shelljs/global");
+var superagent = require("superagent");
+
+exec("ruby -e 'require \"optimus_prime\"; OptimusPrime::Cannon.fire!(7011)'");
+
+module.exports = {
+  count: function(path, callback) {
+    var requestsUrl = "http://localhost:7011/requests/"+ path;
+    superagent.get(requestsUrl, function(e,r) {
+      callback(e, JSON.parse(r.text)["count"]);
+    });
+  },
+
+  prime: function(path) {
+    options = Array.prototype.slice.call(arguments, 1)[0];
+    params = this.merge({path_name: path}, options);
+
+    superagent.post("http://localhost.bskyb.com:7011/prime").
+      send(params).
+      end(function(error, response) {
+        if (response.status === 404) { new Throw("Could not be primed :("); }
+      });
+  }
+};
