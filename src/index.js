36d78cdfb66550aa600f32755034595384387d64
Antonio Nalesso <antonio.nalessomoreira@sky.uk>
2015-06-17 17:54:14 +0100

    AN: If URLToBeReplaced is present it then replaces it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var superagent = require("superagent");
 var Promise = require('es6-promise').Promise;
 
-var OptimusPrimeResolver = function (path, id) {
+var OptimusPrimeResolver = function (path, id, URLToBeReplaced) {
   this.path = path;
   this.id = id;
   this.waitFor = 5000;
+  this._URLToBeReplaced = URLToBeReplaced;
 
   this.count = function(path, callback) {
     var requestsUrl = "http://localhost:7011/requests/"+ path + '?_OpID='+ this.id;
@@ -17,15 +18,21 @@ var OptimusPrimeResolver = function (path, id) {
     var path = this.path;
     var primedId = this.id;
     var self = this;
+    var _URLToBeReplaced = this._URLToBeReplaced;
 
     return new Promise(function (resolve, reject) {
-      browser.executeScript('return window.preregBackendUrl;').then(function(originalURL) {
-        browser.executeScript('window.preregBackendUrl = "'+ (originalURL+"?_OpID="+ primedId) +'";').then(function () {
-          browser.waitForAngular();
-          fn(self);
-          resolve(true);
+      if (_URLToBeReplaced) {
+        browser.executeScript('return '+_URLToBeReplaced+';').then(function(originalURL) {
+          browser.executeScript(_URLToBeReplaced + '= "'+ (originalURL+"?_OpID="+ primedId) +'";').then(function () {
+            browser.waitForAngular();
+            fn(self);
+            resolve(true);
+          });
         });
-      });
+      } else {
+        fn(self);
+        resolve(true);
+      }
     });
   };
 
@@ -73,7 +80,8 @@ module.exports = function (path, options, callback) {
     return tmpObj;
   };
 
-  var id = typeof(callback) === "function" ? '' : Date.now();
+  var id = typeof(callback) === "function" ? '' : parseInt(Math.random(Date.now()) * 1000) + Date.now();
+  var URLToBeReplaced = options._URLToBeReplaced;
 
   if (options === undefined) { options = {}; }
   options.content_type = options.content_type ? options.content_type : 'json'
@@ -93,14 +101,14 @@ module.exports = function (path, options, callback) {
     //
     // We need to wait for angular :)
     //
-    browser.waitForAngular();
+    if (URLToBeReplaced) { browser.waitForAngular(); }
 
     return new Promise(function (resolve, reject) {
       superagent.post("http://localhost.bskyb.com:7011/prime").
         send(params).
         end(function(error, response) {
           if (error) { reject(false); throw new Error("Could not be primed :("); }
-          resolve(new OptimusPrimeResolver(path, id));
+          resolve(new OptimusPrimeResolver(path, id, URLToBeReplaced));
         });
     });
   }
