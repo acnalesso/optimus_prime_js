92cdee8987db7f27c79a70d15104839849ffc248
Antonio Nalesso <antonio.nalessomoreira@sky.uk>
2015-06-24 10:49:31 +0100

    AN/CR: Testing optimus prime WIP

--- test/primer-spec.js
+++ test/primer-spec.js
@@ -1,7 +1,34 @@
-var expect = require('chai').expect;
+var sinonChai = require('sinon-chai');
+var expect = require('chai').use(sinonChai).expect;
+var sinon = require('sinon');
+var prime = require('../src/primer');
+
 
 describe('Primer', function () {
-  it('primes', function () {
-    expect(true).to.equal(true);
+
+  var url, params;
+  prime.requester = sinon.stub(prime.requester, "post", function(mockServerURL) {
+    url = mockServerURL;
+    return {
+      send: function (mockParams) {
+        params = mockParams;
+        return {
+          end: function(callback) {
+            callback();
+          }
+        };
+      }
+    };
+  });
+
+  it('sends a post request to optimus prime mock server', function () {
+    prime('endpoint', {}, function () {});
+    expect(url).to.equal('http://localhost.bskyb.com:7011/prime');
+  });
+
+  it('primes with a callback function', function () {
+    var called = false;
+    prime('endpoint', {}, function () { called = true; });
+    expect(called).to.equal(true);
   });
 });
